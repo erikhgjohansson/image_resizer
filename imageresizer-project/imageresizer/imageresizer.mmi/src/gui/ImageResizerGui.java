a322a9c24d2da777b71665a2f8e4a5d52625a91e
erikhgjohansson <erik.hg.johansson@gmail.com>
2023-05-01 16:47:12 +0200

    fix gui slidervalues

--- imageresizer-project/imageresizer/imageresizer.mmi/src/gui/ImageResizerGui.java
+++ imageresizer-project/imageresizer/imageresizer.mmi/src/gui/ImageResizerGui.java
@@ -6,21 +6,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ServiceLoader;
 
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import api.data.ImageResizingLimits;
 import api.data.ResizingMethod;
 import api.services.ImageResizerService;
 
 public class ImageResizerGui extends JFrame {
+    private static final String PIXELS_STRING = " pixels";
+    private static final String MAXIMUM_HEIGHT_STRING = "Maximum height ";
+    private static final String MAXIMUM_WIDTH_STRING = "Maximum width ";
     private static final int MAX_PIXELS = 512;
     private static final int DEFAULT_MAX_PIXELS = 200;
     private static final long serialVersionUID = 1L;
@@ -32,11 +35,11 @@ public class ImageResizerGui extends JFrame {
         _imageResizerService = loadImageResizerService();
         // service.resizeImage(pathImageToScale);
         panel = new JPanel();
-        panel.setLayout(new GridLayout(5,2));
+        panel.setLayout(new GridLayout(5, 2));
 
         JLabel label = new JLabel("Enter full path to image to be resized");
-        JLabel widthLabel = new JLabel("Maximum width [pixels]");
-        JLabel heightLabel = new JLabel("Maximum height [pixels]");
+        JLabel widthLabel = new JLabel(MAXIMUM_WIDTH_STRING + DEFAULT_MAX_PIXELS + PIXELS_STRING);
+        JLabel heightLabel = new JLabel(MAXIMUM_HEIGHT_STRING + DEFAULT_MAX_PIXELS + PIXELS_STRING);
         JTextField imagePath = new JTextField();
         imagePath.setText("Absolute path");
         imagePath.setColumns(20);
@@ -47,20 +50,30 @@ public class ImageResizerGui extends JFrame {
         width.setName("Width");
         width.setPaintTicks(true);
         width.setPaintLabels(true);
-        width.setMinorTickSpacing(20);  
+        width.setMinorTickSpacing(20);
         width.setMajorTickSpacing(100);
-        
-        
+
+        width.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                widthLabel.setText(MAXIMUM_WIDTH_STRING + ((JSlider) e.getSource()).getValue() + PIXELS_STRING);
+            }
+        });
+
         JSlider height = new JSlider(0, MAX_PIXELS, DEFAULT_MAX_PIXELS);
         height.setName("height");
         height.setPaintTicks(true);
         height.setPaintLabels(true);
-        height.setMinorTickSpacing(20);  
+        height.setMinorTickSpacing(20);
         height.setMajorTickSpacing(100);
-        
+
+        height.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                heightLabel.setText(MAXIMUM_HEIGHT_STRING + ((JSlider) e.getSource()).getValue() + PIXELS_STRING);
+            }
+        });
+
         JComboBox<ResizingMethod> resizingMethod = new JComboBox<>(ResizingMethod.values());
-        
-        
+
         JLabel scalingResult = new JLabel("");
 
         button.addActionListener(new ActionListener() {
